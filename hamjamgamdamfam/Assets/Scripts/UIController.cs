c5d82bde4aa29ac4fc14f9fe3ce39a2628f00800
Robert Butler <robbutler902@gmail.com>
2018-05-04 20:26:01 -0300

    Cursor follows mouse

--- hamjamgamdamfam/Assets/Scripts/UIController.cs
+++ hamjamgamdamfam/Assets/Scripts/UIController.cs
@@ -5,10 +5,38 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
 
+	[SerializeField] protected Canvas uiCanvas;
 	[SerializeField] protected Image reticleImage;
+	[SerializeField] protected Camera worldCamera;
+
+	RectTransform canvasRect;
+	RectTransform reticleRect;
+	Vector2 targetReticlePosition = Vector2.zero;
+	Vector2 reticleVelocity = Vector2.zero;
+
+	void Awake()
+	{
+		canvasRect = uiCanvas.GetComponent<RectTransform>();
+		reticleRect = reticleImage.GetComponent<RectTransform>() ;
+	}
 
 	void Update () 
 	{
-			
+		GetTargetReticlePosition();
+		//reticleRect.anchoredPosition = Vector2.SmoothDamp(reticleRect.anchoredPosition, targetReticlePosition, ref reticleVelocity, 10f, maxSpeed: 10, deltaTime: Time.deltaTime);
+		reticleRect.anchoredPosition = Vector2.MoveTowards(reticleRect.anchoredPosition, targetReticlePosition, 50f);
+	}
+
+	void GetTargetReticlePosition()
+	{
+		var mousePosition = Input.mousePosition;
+		
+		Vector2 viewportPosition = new Vector2(mousePosition.x / Screen.width, mousePosition.y / Screen.height);
+		Vector2 screenPosition = new Vector2(
+			((viewportPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+			((viewportPosition.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f))
+		);
+
+		targetReticlePosition = screenPosition;
 	}
 }
