526e87ddceaf798de38db4808056c72554d48c29
Robert Butler <robbutler902@gmail.com>
2018-05-04 20:02:28 -0300

    UI work

---
+++ hamjamgamdamfam/Assets/Scripts/UIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField] protected Image reticleImage;
+
+	void Update () 
+	{
+			
+	}
+}
