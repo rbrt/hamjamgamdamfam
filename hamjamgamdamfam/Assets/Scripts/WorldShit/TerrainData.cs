01185ad0ccc74aadb01822a480a6255e90b14ea9
simon mcilhargey <simonmcilhargey@simons-MacBook-Pro.local>
2018-05-05 12:05:27 -0300

    spooling sorta, probably mute

---
+++ hamjamgamdamfam/Assets/Scripts/WorldShit/TerrainData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName="TerrainDefault", menuName="Simon/Create Terrain SO", order=1)]
+public class TerrainData : ScriptableObject {
+
+	public Mesh mesh;
+	public Material material;
+
+}
---
+++ hamjamgamdamfam/Assets/Scripts/WorldShit/TerrainInstance.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainInstance : MonoBehaviour {
+	
+	public Renderer meshRenderer;
+	public MeshFilter meshFilter;
+}
