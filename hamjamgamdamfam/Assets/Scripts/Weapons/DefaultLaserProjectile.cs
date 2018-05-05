9d045a2a75129e964c9b70bd1f52c2cac54650f9
Robert Butler <robbutler902@gmail.com>
2018-05-05 11:59:22 -0300

    lasers clean themselves up after a given time

--- hamjamgamdamfam/Assets/Scripts/Weapons/DefaultLaserProjectile.cs
+++ hamjamgamdamfam/Assets/Scripts/Weapons/DefaultLaserProjectile.cs
@@ -6,9 +6,21 @@ public class DefaultLaserProjectile : MonoBehaviour
 {
 
 	float speed = 50;
-	
+	float timeToLive = 10;
+
+	float startTime = 0;
+
+	void Awake()
+	{
+		startTime = Time.time;
+	}
+
 	void Update () 
 	{
+		if (Time.time - startTime > timeToLive)
+		{
+			Destroy(this.gameObject);
+		}
 		transform.position += transform.forward * speed * Time.deltaTime;
 	}
 }
