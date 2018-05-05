72a39ac012b010615a088a946a12989fc62cdcdc
Robert Butler <robbutler902@gmail.com>
2018-05-05 09:40:14 -0300

    Define controller interface

---
+++ hamjamgamdamfam/Assets/Scripts/Controllers/ControllerInterface.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInterface : MonoBehaviour 
+{
+
+	// Return mouse or joystick input
+	public virtual Vector2 GetViewportPosition()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetFireButtonDown()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetSecondaryFireButtonDown()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetBankRightButtonDown()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetBankLeftButtonDown()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetFireButtonUp()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetSecondaryFireButtonUp()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetBankRightButtonUp()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual bool GetBankLeftButtonUp()
+	{
+		throw new System.NotImplementedException();
+	}
+}
--- hamjamgamdamfam/Assets/Scripts/Controllers/UIController.cs
+++ hamjamgamdamfam/Assets/Scripts/Controllers/UIController.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour 
+{
+
+	static UIController instance;
+
+	public static UIController Instance
+	{
+		get 
+		{
+			return instance;
+		}
+	}
 
 	[SerializeField] protected Canvas uiCanvas;
 	[SerializeField] protected Image reticleImage;
@@ -16,6 +27,11 @@ public class UIController : MonoBehaviour {
 
 	void Awake()
 	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+
 		canvasRect = uiCanvas.GetComponent<RectTransform>();
 		reticleRect = reticleImage.GetComponent<RectTransform>() ;
 	}
@@ -23,14 +39,11 @@ public class UIController : MonoBehaviour {
 	void Update () 
 	{
 		GetTargetReticlePosition();
-		//reticleRect.anchoredPosition = Vector2.SmoothDamp(reticleRect.anchoredPosition, targetReticlePosition, ref reticleVelocity, 10f, maxSpeed: 10, deltaTime: Time.deltaTime);
 		reticleRect.anchoredPosition = Vector2.MoveTowards(reticleRect.anchoredPosition, targetReticlePosition, 50f);
 	}
 
-	void GetTargetReticlePosition()
+	public void SetTargetReticlePosition(Vector2 mousePosition)
 	{
-		var mousePosition = Input.mousePosition;
-		
 		Vector2 viewportPosition = new Vector2(mousePosition.x / Screen.width, mousePosition.y / Screen.height);
 		Vector2 screenPosition = new Vector2(
 			((viewportPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
