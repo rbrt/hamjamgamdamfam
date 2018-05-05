5ee0f0d25aba48521728f190a9ab7972883fc2e6
simon mcilhargey <simonmcilhargey@simons-MacBook-Pro.local>
2018-05-04 22:12:10 -0300

    Object spawner

---
+++ hamjamgamdamfam/Assets/Scripts/WorldShit/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	
+	float Speed { get { return Globals.Instance.StaticSpeed; } }
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = transform.position + (Vector3.back * Speed * Time.deltaTime );
+
+		if ( transform.position.z < -10f)
+		{
+			ObjectManager.Instance.Free( this.gameObject);
+		}
+		
+	}
+}
