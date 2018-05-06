f0d85c3bb6333511a648caf9ecf7e9f8cef736ea
Robert Butler <robbutler902@gmail.com>
2018-05-06 17:02:15 -0300

    Info DIsplay UI

---
+++ hamjamgamdamfam/Assets/Scripts/InfoDisplay.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoDisplay : MonoBehaviour 
+{
+
+	static InfoDisplay instance;
+
+	string displayBool = "show";
+	bool displayingWaveScreen = false;
+	bool displayingDeathScreen = false;
+
+	public static InfoDisplay Instance 
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	void Update()
+	{
+		if (displayingWaveScreen)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				DismissWave();
+			}
+		}
+		else if (displayingDeathScreen)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				DismissDeath();
+			}
+		}
+	}
+
+	public void WaveComplete()
+	{
+		displayingWaveScreen = true;
+	}
+
+	public void PlayerDied()
+	{
+
+	}
+
+	void DismissWave()
+	{
+
+	}
+
+	void DismissDeath()
+	{
+
+	}
+}
